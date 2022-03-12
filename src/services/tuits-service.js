4bdc53ba7073bf7e46c2fd5781ef2409222aa554
kunchao322 <chao.k@northeastern.edu>
2022-03-12 15:36:25 -0500

    modified api to heroku

--- src/services/tuits-service.js
+++ src/services/tuits-service.js
@@ -1,7 +1,11 @@
 import axios from "axios";
 
-const TUITS_API = "https://cs5500-01-sp22.herokuapp.com/api/tuits";
-const USERS_API = "https://cs5500-01-sp22.herokuapp.com/api/users";
+// const TUITS_API = "https://cs5500-01-sp22.herokuapp.com/api/tuits";
+// const USERS_API = "https://cs5500-01-sp22.herokuapp.com/api/users";
+
+const TUITS_API = "https://software-engineering-node-kc.herokuapp.com/tuits";
+const USERS_API = "https://software-engineering-node-kc.herokuapp.com/users";
+
 
 export const findAllTuits = () =>
     axios.get(TUITS_API)
